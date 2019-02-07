15728f7a40aceec914f934020fa7d5cc50b8b537
Sylvain Julé <sylvain.jule@free.fr>
2019-02-07 09:30:38 +0100

    Require turkish language

--- config.php
+++ config.php
@@ -8,5 +8,6 @@ Kirby::plugin('sylvainjule/pagetable', array(
         'en' => require_once __DIR__ . '/lib/languages/en.php',
         'de' => require_once __DIR__ . '/lib/languages/de.php',
         'fr' => require_once __DIR__ . '/lib/languages/fr.php',
+        'tr' => require_once __DIR__ . '/lib/languages/tr.php',
     ),
 ));
