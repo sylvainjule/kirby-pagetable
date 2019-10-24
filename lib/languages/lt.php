6369459f7021e4c188b8284f21c73aa9b244341d
romanustin <53936318+romanustin@users.noreply.github.com>
2019-10-24 22:40:08 +0300

    lt.php Lithuanian translation

---
+++ lib/languages/lt.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'pagetable.rowsPerPage'  => 'Rodomi puslapiai',
+	'pagetable.of'           => 'iš',
+	'pagetable.all'          => 'Visi',
+	'pagetable.filter-pages' => 'Filtruoti puslapius…',
+	'pagetable.reset'        => 'Atšaukti',
+);
