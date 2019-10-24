5c55e5d829f048460e5f3a958fe9d8a551d63629
romanustin <53936318+romanustin@users.noreply.github.com>
2019-10-24 22:41:02 +0300

    added Lithuanian translation

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ Kirby::plugin('sylvainjule/pagetable', array(
         'en' => require_once __DIR__ . '/lib/languages/en.php',
         'de' => require_once __DIR__ . '/lib/languages/de.php',
         'fr' => require_once __DIR__ . '/lib/languages/fr.php',
+        'lt' => require_once __DIR__ . '/lib/languages/lt.php',
         'tr' => require_once __DIR__ . '/lib/languages/tr.php',
     ),
 ));
