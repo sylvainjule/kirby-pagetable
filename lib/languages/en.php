cf70ef5b6b8287b23ce0dc41c172ab9fa0538bbc
Sylvain Julé <sylvain.jule@free.fr>
2019-10-31 15:19:32 +0100

    Fix indentation in fr.php / en.php

--- lib/languages/en.php
+++ lib/languages/en.php
@@ -1,10 +1,10 @@
 <?php
 
 return array(
-	'pagetable.rowsPerPage'  => 'Pages displayed',
-	'pagetable.of'           => 'of',
-	'pagetable.all'          => 'All',
-	'pagetable.filter-pages' => 'Filter pages…',
-	'pagetable.reset'        => 'Reset',
+    'pagetable.rowsPerPage'  => 'Pages displayed',
+    'pagetable.of'           => 'of',
+    'pagetable.all'          => 'All',
+    'pagetable.filter-pages' => 'Filter pages…',
+    'pagetable.reset'        => 'Reset',
     'pagetable.empty'        => 'There is no page to display',
 );
--- lib/languages/fr.php
+++ lib/languages/fr.php
@@ -1,10 +1,10 @@
 <?php
 
 return array(
-	'pagetable.rowsPerPage'  => 'Pages affichées',
-	'pagetable.of'           => 'sur',
-	'pagetable.all'          => 'Toutes',
-	'pagetable.filter-pages' => 'Filtrer les pages…',
-	'pagetable.reset'        => 'Réinitialiser',
+    'pagetable.rowsPerPage'  => 'Pages affichées',
+    'pagetable.of'           => 'sur',
+    'pagetable.all'          => 'Toutes',
+    'pagetable.filter-pages' => 'Filtrer les pages…',
+    'pagetable.reset'        => 'Réinitialiser',
     'pagetable.empty'        => 'Il n\'y a aucune page à afficher',
 );
