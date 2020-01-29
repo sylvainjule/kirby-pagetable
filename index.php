98340c49ddd539b9591803b99eada6e51fb2ffa8
Luz <shair.nash@gmail.com>
2020-01-29 10:28:37 -0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ Kirby::plugin('sylvainjule/pagetable', array(
     'translations' => array(
         'en'    => require_once __DIR__ . '/lib/languages/en.php',
         'es_ES' => require_once __DIR__ . '/lib/languages/es.php',
+        'es_419'=> require_once __DIR__ . '/lib/languages/es.php',
         'de'    => require_once __DIR__ . '/lib/languages/de.php',
         'fr'    => require_once __DIR__ . '/lib/languages/fr.php',
         'lt'    => require_once __DIR__ . '/lib/languages/lt.php',
